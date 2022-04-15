8e979905d4a50d96f6353bb466bd7df1cc25513b
user <sivtsovatanya1985@gmail.com>
2022-04-15 12:22:31 +0300

    - added styles to the component ExpenseItem;

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from "./components/Expenses/ExpenseItem";
 
-class App extends Component {
-  render() {
+const App = () => {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+        <ExpenseItem/>
+    )
 }
 
 export default App;
