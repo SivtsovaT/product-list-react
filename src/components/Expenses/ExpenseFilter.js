55580fa25ecdcb30e23479147857cc2d4ae2efc1
user <sivtsovatanya1985@gmail.com>
2022-04-16 19:22:09 +0300

    - implemented the functionality of creating a new object in the form of JSX

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -8,16 +8,14 @@ const ExpenseFilter = (props) => {
     }
 
     return (
-        <div className='expenses-filter'>
-            <div className='expenses-filter__control'>
-                <label>Filter by year</label>
-                <select value={props.selected} onChange={dropdownChangeHandler}>
-                    <option value='2022'>2022</option>
-                    <option value='2021'>2021</option>
-                    <option value='2020'>2020</option>
-                    <option value='2019'>2019</option>
-                </select>
-            </div>
+        <div className='expenses-filter' onChange={dropdownChangeHandler}>
+            <label>Filter by year</label>
+            <select>
+                <option value='2022'>2022</option>
+                <option value='2021'>2021</option>
+                <option value='2020'>2020</option>
+                <option value='2019'>2019</option>
+            </select>
         </div>
     )
 }
